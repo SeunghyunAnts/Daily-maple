ecee8aad1ef9e48780f2f0d146c8d4e6d87a9599
hhyuw <high2092@gmail.com>
2021-01-11 02:04:21 +0900

    Feat: Activity transition by using Intent

--- app/src/main/java/com/example/dailymaple/MainActivity.java
+++ app/src/main/java/com/example/dailymaple/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.dailymaple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button character;
+    Button setting;
+    Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        character = findViewById(R.id.character);
+        character.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity1.class);
+                startActivity(intent);
+            }
+        });
+
+        setting = findViewById(R.id.default_setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        add = findViewById(R.id.add_character);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
