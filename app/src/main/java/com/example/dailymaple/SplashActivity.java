f9c2836ac7efa8222ebc693875fd118f32138716
SeunghyunAnts <seunghyun4525@gmail.com>
2021-01-12 00:37:39 +0900

    feat: Add SplashActivity and Splash animation

---
+++ app/src/main/java/com/example/dailymaple/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.dailymaple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+public class SplashActivity extends AppCompatActivity {
+    // declare
+    private Animation anim;
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // show splash screen
+        @SuppressLint("HandlerLeak") Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message message) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 2000);
+    }
+}
