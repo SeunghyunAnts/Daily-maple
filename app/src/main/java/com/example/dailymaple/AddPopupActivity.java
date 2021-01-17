69f9f287781f9e4e05bbcef4905c9ad075673428
hsh <tmdgus2568@gmail.com>
2021-01-17 22:43:25 +0900

    이미지 크롤링 수정중

--- app/src/main/java/com/example/dailymaple/AddPopupActivity.java
+++ app/src/main/java/com/example/dailymaple/AddPopupActivity.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AddPopupActivity extends Activity implements View.OnClickListener {
     EditText editText_name;
@@ -19,6 +31,7 @@ public class AddPopupActivity extends Activity implements View.OnClickListener {
     Button button_cancel;
     Intent intent;
     Intent intent2;
+    String img_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +54,32 @@ public class AddPopupActivity extends Activity implements View.OnClickListener {
 
         intent2 = getIntent();
 
+        editText_name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                new Thread() {
+                    public void run() {
+                        try {
+                            img_url = crawling("https://maplestory.nexon.com/Ranking/World/Total?c="+s.toString(),s.toString());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+                Glide.with(getApplicationContext()).load(img_url).into(imageView_chr);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -58,5 +97,32 @@ public class AddPopupActivity extends Activity implements View.OnClickListener {
                 finish();
 
         }
+    }
+
+    String crawling(String url, String name) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        ArrayList<Element> elems = new ArrayList<>();
+
+        Elements elem = doc.select("table[class=\"rank_table\"]");
+        Elements elem2 = doc.select("span[class=\"char_img\"]");
+
+        int cnt = 0;
+        int save_dt = 0;
+        int i = 0;
+        for(Element e: elem.select("dt")){
+//            System.out.println("e : " + e.text());
+            if(e.text().equals(name)){
+                save_dt = cnt;
+            }
+            cnt++;
+        }
+        for(Element e: elem.select(".char_img")){
+            if(i == save_dt){
+                return e.select("img[class=\"\"]").attr("src");
+            }
+            i++;
+//            System.out.println("src : "+e.select("img[class=\"\"]").attr("src"));
+        }
+        return "";
     }
 }
