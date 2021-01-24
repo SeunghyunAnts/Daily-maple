5d1c52e05e7acbdd4503d8aaa32a1586cfbc16b0
seunghyunAnt <seunghyun4525@gmail.com>
2021-01-24 20:52:14 +0900

    feat: Add toolbar to ViewProgressActivity

--- app/src/main/java/com/example/dailymaple/ViewProgressActivity.java
+++ app/src/main/java/com/example/dailymaple/ViewProgressActivity.java
@@ -1,10 +1,16 @@
 package com.example.dailymaple;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ViewProgressActivity extends AppCompatActivity {
 
@@ -14,6 +20,14 @@ public class ViewProgressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_progress);
+
+        // 툴바 설정
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         DailyContents = findViewById(R.id.daily);
         DailyContents.setOnClickListener(this::onClickDaily);
     }
@@ -21,5 +35,30 @@ public class ViewProgressActivity extends AppCompatActivity {
     public void onClickDaily(View v) {
         intent = new Intent(this, ViewDailyContentsActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        Log.d("memo", "asdfdsa");
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+            case R.id.action_setting:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(getApplicationContext(), "환경설정 버튼 클릭됨", Toast.LENGTH_LONG).show();
+
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
