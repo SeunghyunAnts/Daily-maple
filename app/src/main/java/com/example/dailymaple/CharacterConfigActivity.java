d83bf0920e00623ecaf08b8781de239ad7d941b8
seunghyunAnt <seunghyun4525@gmail.com>
2021-02-04 22:12:58 +0900

    feat: Connect CharacterConfigActivity

--- app/src/main/java/com/example/dailymaple/CharacterConfigActivity.java
+++ app/src/main/java/com/example/dailymaple/CharacterConfigActivity.java
@@ -1,5 +1,6 @@
 package com.example.dailymaple;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.work.WorkManager;
@@ -11,24 +12,48 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CharacterConfigActivity extends AppCompatActivity {
-    TextView mainCharacterTextView, ursusNotifyNameTextView;
-    ImageView updateCharacterButton;
-    CompoundButton switchActivateUrsusNotify, switchActivateDailyBossNotify, switchActivateWeeklyBossNotify;
+    // layout
+    CompoundButton switchActivateUrsusNotify;
+    Button updateButton;
+    LinearLayout dailyNotifyLinearLayout, weeklyNotifyLinearLayout;
+
+    // user
     String characterID;
+    String platform;
+    String userId;
+
+    // config
+    Boolean character_alert;
+
+    // firestore
+    DocumentReference path;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_config);
 
+        // intent from ViewProgressActivity
         Intent intent = getIntent();
 
         // 툴바 설정
@@ -38,16 +63,63 @@ public class CharacterConfigActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        // 계정 정보 초기화
+        platform = PreferenceHelper.getString(getApplicationContext(), Constants.SHARED_PREF_PLATFORM_KEY);
+        userId = PreferenceHelper.getString(getApplicationContext(), Constants.SHARED_PREF_USER_KEY);
+
         // 캐릭터 정보 초기화
         characterID = intent.getStringExtra("characterId");
 
-        initSwitchLayout(WorkManager.getInstance(getApplicationContext()));
+        // 캐릭터 정보 업데이트 버튼
+        updateButton = findViewById(R.id.character_update_btn);
+
+        // Firestore 경로
+        path = db.collection(platform+"_users")
+                .document(userId)
+                .collection("characters")
+                .document(characterID);
+
+        // 데이터 불러오는 스레드
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    Log.d("thread :", "start thread");
+                    path.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    Log.d("thread : ", "DocumentSnapshot data: " + document.getData().get("daily_contents"));
+                                    character_alert = (Boolean) document.getData().get("total_alert");
+                                    Log.d("thread : ", "save data to character_alert");
+                                    initLayout(WorkManager.getInstance(getApplicationContext()));
+                                } else {
+                                    Log.d("TAG", "No such document");
+                                }
+                            }
+                            else {
+                                Log.d("Error", "get data failed");
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     // 설정 클릭 초기화
-    private void initSwitchLayout(final WorkManager workManager) {
-        // 캐릭터 정보 갱신 버튼 설정
-        updateCharacterButton.setOnClickListener(new View.OnClickListener() {
+    private void initLayout(final WorkManager workManager) {
+//        // 캐릭터 정보 갱신 버튼 설정
+        updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 캐릭터 정보 갱신
@@ -55,72 +127,41 @@ public class CharacterConfigActivity extends AppCompatActivity {
             }
         });
 
+        switchActivateUrsusNotify = (CompoundButton) findViewById(R.id.charater_notify_btn);
+        switchActivateUrsusNotify.setChecked(character_alert);
+
         // 우르스 버튼 설정
         switchActivateUrsusNotify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Log.d("notifi!", "ursus button boolean : true");
-                    boolean isChannelCreated = NotificationHelper.isNotificationChannelCreated(getApplicationContext());
-                    if (isChannelCreated) {
-                        Log.d("notifi!", "channel exist");
-                        PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_URSUS_KEY, true);
-                        NotificationHelper.setScheduledNotification(workManager);
-                    } else {
-                        Log.d("notifi!", "channel not exist");
-                        NotificationHelper.createNotificationChannel(getApplicationContext());
-                    }
+                    // 여기에 이벤트 추가 (알람 설정 등)
                 } else {
-                    Log.d("!", "ursus button boolean : false");
-                    PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_URSUS_KEY, false);
-                    workManager.cancelAllWork();
+                    // 여기에 이벤트 추가 ( "" )
                 }
+                // firestore에 변경사항 업데이트
+                Map<String, Object> data = new HashMap<>();
+                character_alert = !character_alert;
+                data.put("total_alert", character_alert);
+                path.set(data, SetOptions.merge());
             }
         });
 
-        // 일일 보스 알림 설정
-        switchActivateDailyBossNotify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        dailyNotifyLinearLayout = findViewById(R.id.character_daily_contents);
+        dailyNotifyLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Log.d("notifi!", "weekly boss button boolean : true");
-                    boolean isChannelCreated = NotificationHelper.isNotificationChannelCreated(getApplicationContext());
-                    if (isChannelCreated) {
-                        Log.d("notifi!", "channel exist");
-                        PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_DAILY_BOSS_KEY, true);
-                        NotificationHelper.setScheduledNotification(workManager);
-                    } else {
-                        Log.d("notifi!", "channel not exist");
-                        NotificationHelper.createNotificationChannel(getApplicationContext());
-                    }
-                } else {
-                    Log.d("!", "weekly boss button boolean : false");
-                    PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_DAILY_BOSS_KEY, false);
-                    workManager.cancelAllWork();
-                }
+            public void onClick(View v) {
+                // 클릭시 일일 컨텐츠 설정 팝업으로 이동
+                Intent intent = new Intent();
             }
         });
 
-        // 주간 보스 알림 설정
-        switchActivateWeeklyBossNotify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        weeklyNotifyLinearLayout = findViewById(R.id.character_weekly_contents);
+        weeklyNotifyLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Log.d("notifi!", "weekly boss button boolean : true");
-                    boolean isChannelCreated = NotificationHelper.isNotificationChannelCreated(getApplicationContext());
-                    if (isChannelCreated) {
-                        Log.d("notifi!", "channel exist");
-                        PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_WEEKLY_BOSS_KEY, true);
-                        NotificationHelper.setScheduledNotification(workManager);
-                    } else {
-                        Log.d("notifi!", "channel not exist");
-                        NotificationHelper.createNotificationChannel(getApplicationContext());
-                    }
-                } else {
-                    Log.d("!", "weekly boss button boolean : false");
-                    PreferenceHelper.setBoolean(getApplicationContext(), Constants.SHARED_PREF_WEEKLY_BOSS_KEY, false);
-                    workManager.cancelAllWork();
-                }
+            public void onClick(View v) {
+                // 클릭시 주간 컨텐츠 설정 팝업으로 이동
+                Intent intent = new Intent();
             }
         });
     }
@@ -129,22 +170,14 @@ public class CharacterConfigActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
+            // 일일 컨텐츠 팝업 결과
             if (resultCode == RESULT_OK) {
-                //데이터 받기
-                int result = data.getIntExtra("main_character", 0);
-                Log.d("onActivityResult", "ok :" + Integer.toString(result));
 
-//                currentCharacterName = characterInfos.get(result).getNickname();
+            }
+        } else if (requestCode == 2) {
+            // 주간 컨텐츠 팝업 결과
+            if (requestCode == RESULT_OK) {
 
-                // 새로운 창에서 대표 캐릭터 선택
-//                PreferenceHelper.setString(getApplicationContext(), Constants.SHARED_PREF_MAIN_CHARACTER_KEY, currentCharacterName);
-
-                // 대표 캐릭터 변경 후 환경설정 창 UI 변경
-                switchActivateUrsusNotify.setEnabled(true);
-                ursusNotifyNameTextView.setTextColor(Color.parseColor("#000000"));
-
-                // 이후 대표캐릭터 닉네임 변경
-                mainCharacterTextView.setText(PreferenceHelper.getString(getApplicationContext(), Constants.SHARED_PREF_MAIN_CHARACTER_KEY));
             }
         }
     }
